0880d9552f4b54af4c356e38502ca8c1e1c9960d
jinnykaew <42741941+jinnykaew@users.noreply.github.com>
2020-11-29 11:16:15 +0700

    Update HomeWork2.java

--- HomeWork2.java
+++ HomeWork2.java
@@ -4,19 +4,23 @@ public class HomeWork2 {
 
 	public static void main(String[] args) {
 		String[][] table = {
-				{"1","2","3"},
-				{"4","5","6"},
-				{"7","8","9"},
-				
+				{"1","2","3"},{"4","5","6"},{"7","8","9"}
 		};
+		multiplyTable(table);
 		
 		
-
 	}
-
-	public static void multiplyTable(String table) {
+	
+	public void multiplyTable(String[][] input) {
+	
+		for(int i =0;i<input.length;i++) {
+			for(int j = 0;j<input[i].length;j++) {
+				System.out.print(Integer.parseInt(n[i][j])*2);
+			}
+			System.out.println();
+		}
+		
 	}
-
 	
 	
 }
